c814b26e96406344d2d6f05bc337edc0bb385197
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-05-28 00:24:00 +0200

    added a root logger to the library

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
 """Library of the Pytorch-Flows project"""
+import logging
+
+# Ensure that no submodule loggers outputs anything, unless explicitly setup by the user
+logging.getLogger(__name__).addHandler(logging.NullHandler())
