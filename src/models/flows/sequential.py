01948ef2cbe7c34a06649adbc611aeaddaf80847
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-05-27 18:21:22 +0200

    debugged the sequential flow models

--- src/models/flows/sequential.py
+++ src/models/flows/sequential.py
@@ -1,3 +1,4 @@
+import torch
 from .general_flow import GeneralFlow
 
 
@@ -5,7 +6,7 @@ class InvertibleSequentialFlow(GeneralFlow):
     def __init__(self,d,flows):
         super(InvertibleSequentialFlow, self).__init__(d=d)
 
-        self.flows = list(flows)
+        self.flows = torch.nn.ModuleList(flows)
 
         for flow in flows:
             try:
@@ -18,18 +19,28 @@ class InvertibleSequentialFlow(GeneralFlow):
 
     def invert(self):
         self.inverse = not self.inverse
-        self.flows.reverse()
         for flow in self.flows:
             flow.invert()
 
     def flow(self, x):
         output = x
-        for f in self.flows:
-            output = f.flow(output)
+        if self.inverse:
+            for f in self.flows[::-1]:
+                output = f.flow(output)
+        else:
+            for f in self.flows:
+                output = f.flow(output)
+
         return output
+
 
     def transform_and_compute_jacobian(self, xj):
         output = xj
-        for f in self.flows:
-            output = f.transform_and_compute_jacobian(output)
+        if self.inverse:
+            for f in self.flows[::-1]:
+                output = f.transform_and_compute_jacobian(output)
+        else:
+            for f in self.flows:
+                output = f.transform_and_compute_jacobian(output)
+
         return output
