33b81a4e63fa787bc976d6d23f9f8d4acb4544c5
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-06-26 18:21:52 +0200

    cleaning up transformations

--- src/models/flows/coupling_cells/real_nvp.py
+++ src/models/flows/coupling_cells/real_nvp.py
@@ -19,7 +19,7 @@ def element_wise_affine(x, st, compute_jacobian=True):
     if compute_jacobian:
         logj = torch.sum(torch.log(es), dim=-1)
 
-    return es*x + t, logj
+    return es * x + t, logj
 
 
 def inverse_element_wise_affine(x, st, compute_jacobian=True):
@@ -36,7 +36,7 @@ def inverse_element_wise_affine(x, st, compute_jacobian=True):
     if compute_jacobian:
         logj = torch.sum(torch.log(es), dim=-1)
 
-    return es*(x - t), logj
+    return es * (x - t), logj
 
 
 class ElementWiseAffineTransform(InvertibleTransform):
@@ -49,17 +49,19 @@ class ElementWiseAffineTransform(InvertibleTransform):
 
 class GeneralRealNVP(InvertibleCouplingCell):
     """Coupling cell based on affine transforms without a specific parameter prediction function"""
+
     def __init__(self, *, d, mask):
         super(GeneralRealNVP, self).__init__(d=d, transform=ElementWiseAffineTransform(), mask=mask)
 
 
 class FakeT(torch.nn.Module):
     """Fake neural net for the test FakeRealNVP that returns a trainable constant"""
+
     def __init__(self, s=0., t=0.):
         super(FakeT, self).__init__()
-        self.st = torch.nn.Parameter(torch.tensor([s, t]),requires_grad=True)
+        self.st = torch.nn.Parameter(torch.tensor([s, t]), requires_grad=True)
 
-    def forward(self,x):
+    def forward(self, x):
         return self.st
 
 
@@ -67,6 +69,7 @@ class FakeRealNVP(GeneralRealNVP):
     """Fake real NVP with position independent affine parameters
     for basic debugging and training tests
     """
+
     def __init__(self, *, d, mask, s=0., t=0.):
         super(FakeRealNVP, self).__init__(d=d, mask=mask)
         self.T = FakeT(s, t)
@@ -80,20 +83,16 @@ class RealNVP(GeneralRealNVP):
                  hidden_activation=torch.nn.ReLU,
                  output_activation=None,
                  use_batch_norm=False):
-    
-        super(RealNVP, self).__init__(d=d,mask=mask)
+        super(RealNVP, self).__init__(d=d, mask=mask)
 
         d_in = sum(mask)
         d_out = d - d_in
 
         self.T = ArbitraryShapeRectangularDNN(d_in=d_in,
-                                              out_shape=(d_out,2),
+                                              out_shape=(d_out, 2),
                                               d_hidden=d_hidden,
                                               n_hidden=n_hidden,
                                               input_activation=input_activation,
                                               hidden_activation=hidden_activation,
                                               output_activation=output_activation,
                                               use_batch_norm=use_batch_norm)
-
-
-
--- src/models/flows/coupling_cells/transforms.py
+++ src/models/flows/coupling_cells/transforms.py
@@ -1,26 +1,93 @@
 """Abstract definition of invertible transforms for coupling cells"""
 from better_abc import ABC,abstractmethod
+import torch
 
 
 class InvertibleTransform(ABC):
-    """Callable invertible transform which calls self.forward or self.backward depending on self.inverse"""
+    """Callable invertible transform which calls self.forward or self.backward depending on self.inverse
+
+    Attributes
+    ----------
+    inverse: bool
+        whether calling the object will use the forward or backward transform
+    """
     def __init__(self):
+        """Constructor for InvertibleTransform
+
+        No arguments, no returns, this is just encapsulation for a function and its inverse
+        """
         self.inverse = False
 
     @abstractmethod
     def forward(self, y, T, compute_jacobian=True):
+        """Abstract forward transform
+
+        Parameters
+        ----------
+        y: torch.Tensor
+            batch of points to transform
+        T: torch.Tensor
+            batch of parameters for the transformation
+        compute_jacobian: bool
+            whether to compute the jacobian or not
+
+        Returns
+        -------
+        tuple of torch.Tensor, torch.Tensor
+            `(z,j)` where the first tensor is the batch of transformed points and the second is the batch of jacobians
+        """
         pass
 
     @abstractmethod
     def backward(self, y, T, compute_jacobian=True):
+        """Abstract backward transform
+
+        Parameters
+        ----------
+        y: torch.Tensor
+            batch of points to transform
+        T: torch.Tensor
+            batch of parameters for the transformation
+        compute_jacobian: bool
+            whether to compute the jacobian or not
+
+        Returns
+        -------
+        tuple of torch.Tensor, torch.Tensor
+            `(z,j)` where the first tensor is the batch of transformed points and the second is the batch of jacobians
+        """
         pass
 
     def __call__(self,y , T, compute_jacobian=True):
+        """A transform is callable and either performs the forward or inverse transformation
+        depending on the attribute `self.inverse`.
+
+        Parameters
+        ----------
+        y: torch.Tensor
+            batch of points to transform
+        T: torch.Tensor
+            batch of parameters for the transformation
+        compute_jacobian: bool
+            whether to compute the jacobian or not
+
+
+        Returns
+        -------
+        tuple of torch.Tensor, torch.Tensor
+            `(z,j)` where the first tensor is the batch of transformed points and the sec
+        """
         if self.inverse:
             return self.backward(y, T)
         else:
             return self.forward(y, T)
 
     def invert(self):
+        """Flip the `inverse` attribute
+
+        Returns
+        -------
+        None
+        """
         self.inverse = not self.inverse
 
