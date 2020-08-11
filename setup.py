bdfa76edf85d36a8c48fb2d90de44a4fb7b15853
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-08-11 19:25:08 +0200

    improved repo and setup.py dependency list

--- setup.py
+++ setup.py
@@ -3,8 +3,18 @@ from setuptools import find_packages, setup
 setup(
     name='zunis',
     packages=find_packages(),
+    install_requires=[
+        "numpy == 1.19.1",
+        "pandas == 1.1.0",
+        "torch == 1.6.0",
+        "better-abc @ git+https://github.com/ndeutschmann/better_abc@6b393a2ae82da68e85f9a960eecbbb98bfd8d143",
+    ],
     version='0.1.0',
     description='Neural Importance Sampling',
+    long_description=open("README.md").read(),
     author='Nicolas Deutschmann',
+    author_email="nicolas.deutschmann@gmail.com",
+    url="https://ndeutschmann.github.io/zunis/",
+    download_url="https://github.com/ndeutschmann/zunis",
     license='MIT',
 )
