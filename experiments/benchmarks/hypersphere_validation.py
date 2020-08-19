1316ccd3457d8dadca53e9b486770b60f38a4ab6
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-08-19 15:29:31 +0200

    new validation scripts

--- experiments/benchmarks/hypersphere_validation.py
+++ experiments/benchmarks/hypersphere_validation.py
@@ -1,8 +1,19 @@
+import sys
+import logging
 import numpy as np
 import pandas as pd
+import torch
 import matplotlib.pyplot as plt
 from utils.integrands import HypersphereVolumeIntegrand
 from utils.flat_integrals import validate_known_integrand_flat
+
+
+from zunis import logger_integration, logger_training
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(levelname)s: %(message)s [%(name)s]')
+logger_integration.setLevel(logging.WARNING)
+logger_training.setLevel(logging.WARNING)
+logger = logging.getLogger("flat_hypersphere_validation")
 
 results = pd.DataFrame()
 n_batch = 10000000
@@ -11,15 +22,25 @@ dimensions = range(2,10)
 radii = np.linspace(0.48,0.2,20)
 centers = [0.5]
 
+
+if torch.has_cuda:
+    device = torch.device("cuda:7")
+    logger.warning("Using CUDA:7")
+else:
+    device = torch.device("cpu")
+    logger.warning("Using CPU")
+
 for d in dimensions:
     for r in radii:
         for c in centers:
-            f = HypersphereVolumeIntegrand(d, r, c)
-            result = validate_known_integrand_flat(f, d, n_batch)
+            logger.info(f"Validating flat hypersphere for d={d} and r={r:.2f}")
+            f = HypersphereVolumeIntegrand(d, r, c, device=device)
+            result = validate_known_integrand_flat(f, d, n_batch, device=device)
             result["dim"] = d
             result["radius"] = r
             result["center"] = c
             results = pd.concat([results, result.as_dataframe()], ignore_index=True)
+            print("")
 
 fig, ax = plt.subplots()
 ax.scatter(results["dim"], results["percent_difference"])
---
+++ experiments/benchmarks/integrator_validation.py
@@ -0,0 +1,48 @@
+import logging
+import sys
+import torch
+from functools import partial
+
+from zunis.integration import Integrator
+from utils.integrands import HyperrectangleVolumeIntegrand, HypersphereVolumeIntegrand
+from utils.integrator_integrals import validate_integral_integrator
+
+from zunis import logger_integration, logger_training
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(levelname)s: %(message)s [%(name)s]')
+logger_integration.setLevel(logging.WARNING)
+logger_training.setLevel(logging.WARNING)
+logger = logging.getLogger("integrator_validation")
+
+if torch.has_cuda:
+    device = torch.device("cuda:7")
+    logger.warning("Using CUDA:7")
+else:
+    device = torch.device("cpu")
+    logger.warning("Using the CPU")
+
+
+def run_hyperrect_test(d):
+    return
+    logger.info(f"Running hyperrectangle test in {d} dimensions")
+    half = HyperrectangleVolumeIntegrand(d=d)
+    integrator = Integrator(d=d, f=half, device=device)
+    result = validate_integral_integrator(half, integrator, n_batch=100000)
+    print("")
+
+
+def run_hypersphere_test(d):
+    logger.info(f"Running hypersphere test in {d} dimensions")
+    sphere = HypersphereVolumeIntegrand(d=d, r=0.3, c=0.5, device=device)
+    integrator = Integrator(d=d, f=sphere, device=device, trainer_options={"optim": partial(torch.optim.Adam, lr=1.e-4)})
+    result = validate_integral_integrator(sphere, integrator, n_batch=100000)
+    print("")
+    return result
+
+
+if __name__ == "__main__":
+    for d in [2, 3, 4]:
+        run_hyperrect_test(d)
+
+    for d in [2, 3, 4]:
+        run_hypersphere_test(d)
