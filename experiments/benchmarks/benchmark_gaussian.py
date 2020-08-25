6d32fded3237a8cdf493df8417e822f63ce1b632
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-08-25 18:10:27 +0200

    cleaner benchmark interface and logging

--- experiments/benchmarks/benchmark_gaussian.py
+++ experiments/benchmarks/benchmark_gaussian.py
@@ -1,9 +1,12 @@
 """Comparing ZuNIS to VEGAS on gaussian integrals (VEGAS expected to be better)"""
 
 import vegas
-from functools import partial
+import click
+import logging
 import pandas as pd
 import torch
+
+from utils.command_line_tools import PythonLiteralOption
 from utils.integrands.gaussian import DiagonalGaussianIntegrand
 from utils.logging import get_benchmark_logger, get_benchmark_logger_debug
 from utils.torch_utils import get_device
@@ -11,56 +14,77 @@ from utils.integrator_integrals import evaluate_integral_integrator
 from utils.vegas_integrals import evaluate_integral_vegas
 from utils.flat_integrals import evaluate_integral_flat
 from utils.integral_validation import compare_integral_result
+import utils.config as conf
 from zunis.integration import Integrator
 
-#############################################################
-#       DEBUG FLAG: set to False to log and save to file
-#############################################################
-debug = True
-#############################################################
 
-
-if debug:
-    logger = get_benchmark_logger_debug("benchmark_gaussian")
-else:
-    logger = get_benchmark_logger("benchmark_gaussian")
-
-device = get_device(cuda_ID=0)
-
-
-def benchmark_gaussian(d, s=0.3):
+def benchmark_gaussian(d, s=0.3, n_batch=100000, logger=None, device=torch.device("cpu")):
+    if logger is None:
+        get_benchmark_logger_debug("benchmark_gaussian")
     logger.info(f"Benchmarking a gaussian with d={d} and s={s:.1f}")
+
     gaussian = DiagonalGaussianIntegrand(d=d, device=device, s=s)
 
     @vegas.batchintegrand
     def vgaussian(x):
         return gaussian(torch.tensor(x).to(device)).cpu()
 
-    integrator = Integrator(d=d, f=gaussian, device=device, flow_options={"masking_options": {"repetitions": 2},
-                                                                          "cell_params": {"n_bins": 50}})
+    integrator_config = conf.get_default_integrator_config()
+    integrator_config["n_points_survey"] = 100000
+    integrator_config["flow_options"]["cell_params"]["n_bins"] = 50
+    integrator_config["trainer_options"]["n_epochs"] = 50
+    integrator_args = conf.create_integrator_args(integrator_config)
+
+    integrator = Integrator(d=d, f=gaussian, device=device, **integrator_args)
+
     vintegrator = vegas.Integrator([[0, 1]] * d, max_nhcube=1)
 
-    integrator_result = evaluate_integral_integrator(gaussian, integrator, n_batch=100000,
-                                                     survey_args={"n_points": 100000,
-                                                                  "n_epochs": 100})
-    vegas_result = evaluate_integral_vegas(vgaussian, vintegrator, n_batch=100000, n_batch_survey=100000)
-    flat_result = evaluate_integral_flat(gaussian, d, n_batch=100000, device=device)
+    integrator_result = evaluate_integral_integrator(gaussian, integrator, n_batch=n_batch)
+    vegas_result = evaluate_integral_vegas(vgaussian, vintegrator, n_batch=n_batch,
+                                           n_batch_survey=integrator_args["n_points_survey"])
+    flat_result = evaluate_integral_flat(gaussian, d, n_batch=n_batch, device=device)
+
     result = compare_integral_result(integrator_result, vegas_result, sigma_cutoff=3).as_dataframe()
     result["flat_variance_ratio"] = (flat_result["value_std"] / result["value_std"]) ** 2
 
     result["d"] = d
     result["s"] = s
 
+    flat_config = conf.flatten_config(integrator_config)
+    result = result.merge(
+        pd.DataFrame([flat_config.values()], columns=flat_config.keys()),
+        left_index=True,
+        right_index=True
+    )
+
     return result
 
 
-if __name__ == "__main__":
+def run_benchmark(dimensions, sigmas, debug, cuda):
+    if debug:
+        logger = get_benchmark_logger_debug("benchmark_gaussian", zunis_integration_level=logging.DEBUG, zunis_training_level=logging.DEBUG, zunis_level=logging.DEBUG)
+    else:
+        logger = get_benchmark_logger("benchmark_gaussian")
+
+    device = get_device(cuda_ID=cuda)
+
     results = pd.DataFrame()
-    for d in [2, 4, 6, 8, 10]:
-        for s in [0.5, 0.3, 0.1]:
-            result = benchmark_gaussian(d, s)
+    for d in dimensions:
+        for s in sigmas:
+            result = benchmark_gaussian(d, s, logger=logger, device=device)
             results = pd.concat([results, result], ignore_index=True)
 
     print(results)
     if not debug:
         results.to_pickle("benchmark_gaussian.bz2")
+
+
+cli = click.Command("cli", callback=run_benchmark, params=[
+    PythonLiteralOption(["--dimensions"], default=[2, 4, 6, 8, 10]),
+    PythonLiteralOption(["--sigmas"], default=[0.5, 0.3, 0.1]),
+    click.Option(["--debug/--no-debug"], default=True),
+    click.Option(["--cuda"], default=0, type=int)
+])
+
+if __name__ == '__main__':
+    cli()
---
+++ experiments/benchmarks/utils/command_line_tools.py
@@ -0,0 +1,20 @@
+"""Tools for command line interfaces with click"""
+import ast
+
+import click
+
+
+class PythonLiteralOption(click.Option):
+    """Option allowing a python literal to be passed as a string which is then parsed"""
+
+    def type_cast_value(self, ctx, value):
+        try:
+            # Convert the literal
+            return ast.literal_eval(value)
+        except ValueError:
+            # The default value is allowed to not be a litteral for readability
+            # Manifest by the fact that value does not have string type
+            return value
+        except SyntaxError:
+            # Raise a proper error if the string is not parsable
+            raise click.BadParameter(value)
