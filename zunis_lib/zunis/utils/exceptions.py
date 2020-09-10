aa7297cb98081962959901c3bd3fbfd3f4c720d9
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-09-10 11:11:27 +0200

    added GPU-friendly nan-avoidance in PWlinear bin indexing

---
+++ zunis_lib/zunis/utils/exceptions.py
@@ -0,0 +1,11 @@
+"""Custom exceptions for the ZüNIS library"""
+
+
+class AvertedCUDARuntimeError(RuntimeError):
+    """Prevents the raising of a RuntimeError cause by a CUDA device-side assert failure
+    which whould lock the GPU in a failed state."""
+
+
+class NoCheckpoint(RuntimeError):
+    """Raised to signify no checkpoint is available for reloading"""
+
