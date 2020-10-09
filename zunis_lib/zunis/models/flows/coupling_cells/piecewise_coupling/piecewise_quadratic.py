fa4869886e3e4bf8594f62e8e3ea3e0dcbda5084
NGoetz <goetz.niklas@googlemail.com>
2020-10-09 18:21:22 +0200

    extend comments

--- zunis_lib/zunis/models/flows/coupling_cells/piecewise_coupling/piecewise_quadratic.py
+++ zunis_lib/zunis/models/flows/coupling_cells/piecewise_coupling/piecewise_quadratic.py
@@ -65,23 +65,26 @@ def piecewise_quadratic_transform(x, wv_tilde, compute_jacobian=True):
     wsum_shift=torch.cat((torch.zeros([wsum.shape[0],wsum.shape[1],1]).to(wsum.device, wsum.dtype),wsum),axis=-1)
     
     v=modified_softmax(v_tilde, w)
-   
     
     #tensor of shape (N,k,b+1) with 0 entry if x is smaller than the cumulated w and 1 if it is bigger
-    #used to find the bin in which x lies
+    #this is used to find the bin with the number mx in which x lies; for this, the sum of the bin 
+    #widths w has to be smaller to the left and bigger to the right
     finder=torch.where(wsum>torch.unsqueeze(x,axis=-1),torch.zeros_like(wsum),torch.ones_like(wsum))
-    
     eps = torch.finfo(wsum.dtype).eps
-    mx=torch.unsqueeze(torch.argmax(torch.cat((torch.ones([wsum.shape[0],wsum.shape[1],1]).to(wsum.device, wsum.dtype)*eps,finder*wsum),axis=-1),axis=-1),-1)
-    
-    #b+1 refers to x being in the first bin
-    mx=torch.where(mx==finder.shape[2],torch.zeros_like(mx),mx)
+    #the bin number can be extracted by finding the last index for which finder is nonzero. As wsum 
+    #is increasing, this can be found by searching for the maximum entry of finder*wsum. In order to 
+    #get the right result when x is in the first bin and finder is everywhere zero, a small first entry 
+    #is added
+    mx=torch.unsqueeze(  #we need to unsqueeze for later operations
+        torch.argmax( #we search for the maximum in order to find the last bin for which x was greater than wsum
+            torch.cat((torch.ones([wsum.shape[0],wsum.shape[1],1]).to(wsum.device, wsum.dtype)*eps,finder*wsum),
+                      axis=-1),  #we add an offset to ensure that if x is in the first bin, a maximal argument is found 
+            axis=-1), 
+        axis=-1)
    
     # x is in the mx-th bin: x \in [0,1],
     # mx \in [[0,b-1]], so we clamp away the case x == 1
-    
     mx = torch.clamp(mx, 0, b - 1).to(torch.long)
-    
     # Need special error handling because trying to index with mx
     # if it contains nans will lock the GPU. (device-side assert triggered)
     if torch.any(torch.isnan(mx)).item() or torch.any(mx < 0) or torch.any(mx >= b):
@@ -114,8 +117,15 @@ def piecewise_quadratic_transform(x, wv_tilde, compute_jacobian=True):
     #the derivative of this transformation is the linear interpolation between v_i-1 and v_i at alpha
     #the jacobian is the product of all linear interpolations
     if compute_jacobian:
-        logj=torch.squeeze(torch.log(torch.unsqueeze(torch.prod(torch.lerp(torch.squeeze(torch.gather(v,-1,mx),axis=-1),
-                                                torch.squeeze(torch.gather(v,-1,mx+1),axis=-1),alphas), axis=-1),axis=-1)),-1)
+        logj=torch.squeeze(
+            torch.log(torch.unsqueeze(
+                    torch.prod(#we need to take the product over all transformed dimensions
+                        torch.lerp(torch.squeeze(torch.gather(v,-1,mx),axis=-1),
+                                   torch.squeeze(torch.gather(v,-1,mx+1),axis=-1),alphas),
+                        #linear extrapolation between alpha, mx and mx+1
+                    axis=-1),
+                axis=-1)),
+            axis=-1)
        
     # Regularization: points must be strictly within the unit hypercube
     # Use the dtype information from pytorch
@@ -179,7 +189,7 @@ def piecewise_quadratic_inverse_transform(y, wv_tilde, compute_jacobian=True):
     #need to find the bin number for each of the y/x
     #-> find the last bin such that y is greater than the constant of the quadratic equation
     
-    #vw_i= (v_i+1 - v_i)w_i/2 where i is the bin index
+    #vw_i= (v_i+1 - v_i)w_i/2 where i is the bin index. VW is the constant of the quadratic equation
     vw=torch.cat((torch.zeros([v.shape[0],v.shape[1],1]).to(wsum.device, wsum.dtype),
                                   torch.cumsum(torch.mul((v[:,:,:-1]+v[:,:,1:])/2,w),axis=-1)),axis=-1)
     # finder is contains 1 where y is smaller then the constant and 0 if it is greater
@@ -189,7 +199,12 @@ def piecewise_quadratic_inverse_transform(y, wv_tilde, compute_jacobian=True):
     #is increasing, this can be found by searching for the maximum entry of finder*vw. In order to 
     #get the right result when y is in the first bin and finder is everywhere zero, a small first entry 
     #is added and mx is reduced by one to account for the shift.
-    mx=torch.unsqueeze(torch.argmax(torch.cat((torch.ones([vw.shape[0],vw.shape[1],1]).to(vw.device, vw.dtype)*eps,finder*(vw+1)),axis=-1),axis=-1),-1)-1
+    mx=torch.unsqueeze(
+        torch.argmax(#we search for the maximum in order to find the last bin for which y was greater than vw
+            torch.cat((torch.ones([vw.shape[0],vw.shape[1],1]).to(vw.device, vw.dtype)*eps,finder*(vw+1)),axis=-1),
+            axis=-1), #we add an offset to ensure that if x is in the first bin, a maximal argument is found
+        axis=-1)-1 # we substract -1 to account for the offset
+    
     # x is in the mx-th bin: x \in [0,1],
     # mx \in [[0,b-1]], so we clamp away the case x == 1
     edges = torch.clamp(mx, 0, b - 1).to(torch.long)
@@ -200,9 +215,13 @@ def piecewise_quadratic_inverse_transform(y, wv_tilde, compute_jacobian=True):
         raise AvertedCUDARuntimeError("NaN detected in PWQuad bin indexing")
     
     #solve quadratic equation
+    
+    #prefactor of quadratic term
     a=torch.squeeze(torch.mul(torch.gather(v,-1, edges+1)-torch.gather(v,-1, edges),
                                                         torch.gather(w,-1,edges)),axis=-1)
+    #prefactor of linear term
     b=torch.mul(torch.squeeze(torch.gather(v,-1,edges),axis=-1),torch.squeeze(torch.gather(w,-1,edges),axis=-1))
+    #constant - y
     c= torch.squeeze(torch.gather(vw,-1,edges),axis=-1)-y
     
     #ensure that division by zero is taken care of
@@ -234,7 +253,7 @@ def piecewise_quadratic_inverse_transform(y, wv_tilde, compute_jacobian=True):
     )
     
     #the solution is the relative position inside the bin. This can be
-    #converted to the absolute position
+    #converted to the absolute position by adding the sum of the bin widths up to this bin
     x=torch.mul(torch.squeeze(torch.gather(w,-1,edges),axis=-1), sol)+torch.squeeze(torch.gather(wsum_shift,-1,edges),axis=-1)
     
     eps = torch.finfo(x.dtype).eps
@@ -245,8 +264,13 @@ def piecewise_quadratic_inverse_transform(y, wv_tilde, compute_jacobian=True):
     )
     
     if compute_jacobian:
-        logj =-torch.squeeze(torch.log(torch.unsqueeze(torch.prod(torch.lerp(torch.squeeze(torch.gather(v,-1,edges),axis=-1),
-                                                torch.squeeze(torch.gather(v,-1,edges+1),axis=-1),sol), axis=-1),axis=-1)),-1)
+        logj =-torch.squeeze(torch.log(
+            torch.unsqueeze(torch.prod(#we have to take the product of the jacobian of all dimensions
+                torch.lerp(torch.squeeze(torch.gather(v,-1,edges),axis=-1),torch.squeeze(torch.gather(v,-1,edges+1),
+                                                                                         axis=-1),sol),
+                axis=-1), #linear extrapolation between sol, edges and edges+1 gives the jacobian of the forward transformation. The prefactor of -1 is the log of the jacobian of the inverse
+            axis=-1)),
+        axis=-1)
        
     return x.detach(), logj
 
