bdf55fdaa9bb4d53aa27da377a023697d6c6b539
Niklas Götz <goetz@fias.uni-frankfurt.de>
2020-11-29 12:12:09 +0100

    Bugfix and clearified formulation of import paths

--- experiments/benchmarks/benchmark_madgraph/exec_benchmark.sh
+++ experiments/benchmarks/benchmark_madgraph/exec_benchmark.sh
@@ -7,6 +7,7 @@
 
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
+benchmark_path=$(dirname $dir_path)
 ./install_mg.sh
 export LD_LIBRARY_PATH=$dir_path/MG5_aMC_v2_8_2/HEPTools/lhapdf6_py3/lib
 if [ -d "fortran_output" ]; then rm -Rf ./fortran_output; fi
@@ -18,11 +19,11 @@ for f in *;
         make matrix2py.so 2>&1> f2py_log.txt 2>&1;
         rm -f matrix2py.so
         mv matrix2py* matrix2py.so
-        mkdir -p ../../../../utils/integrands/mg/$f/param 
-        cp ../../Cards/param_card.dat ../../../../utils/integrands/mg/$f/param 
-        cp {nexternal.inc,pmass.inc,param.log} ../../../../utils/integrands/mg/$f/param 
-        cp matrix2py.so ../../../../utils/integrands/mg/$f
-        python3 ../../../../benchmark_madgraph.py --process $f --pdf_type NNPDF23_nlo_as_0119   --pdf_dir ../../../MG5_aMC_v2_8_2/HEPTools/lhapdf6_py3/share/LHAPDF --db madgraph_$f --experiment_name madgraph_$f --config ../../../../benchmark_config_examples/madgraph_grid_config.yaml --debug --lhapdf_dir ../../../MG5_aMC_v2_8_2/HEPTools/lhapdf6_py3/lib/python3.*/site-packages --pdf
+        mkdir -p $benchmark_path/utils/integrands/mg/$f/param 
+        cp $dir_path/fortran_output/Cards/param_card.dat $benchmark_path/utils/integrands/mg/$f/param 
+        cp {nexternal.inc,pmass.inc,param.log} $benchmark_path/utils/integrands/mg/$f/param 
+        cp matrix2py.so $benchmark_path/utils/integrands/mg/$f
+        python3 $benchmark_path/benchmark_madgraph.py --process $f --pdf_type NNPDF23_nlo_as_0119   --pdf_dir $dir_path/MG5_aMC_v2_8_2/HEPTools/lhapdf6_py3/share/LHAPDF --db madgraph_$f --experiment_name madgraph_$f --config $benchmark_path/benchmark_config_examples/madgraph_grid_config.yaml --debug --lhapdf_dir $dir_path/MG5_aMC_v2_8_2/HEPTools/lhapdf6_py3/lib/python3.*/site-packages --pdf
         mv madgraph_$f $dir_path/
         cd ..;
     fi
