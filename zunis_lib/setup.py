979041ed606059bb9567e9fa0e55a196c73765d2
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2021-03-01 17:58:15 +0100

    updated dictwrapper dependency to 1.3

--- zunis_lib/setup.py
+++ zunis_lib/setup.py
@@ -8,9 +8,9 @@ setup(
         "pandas == 1.1.0",
         "torch == 1.6.0",
         "abc_property == 1.0",
-        "dictwrapper == 1.0"
+        "dictwrapper == 1.3"
     ],
-    version='0.2rc2',
+    version='0.2rc3',
     description='Neural Importance Sampling',
     long_description=open("../README.md").read(),
     long_description_content_type="text/markdown",
