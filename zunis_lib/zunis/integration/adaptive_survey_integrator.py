fb386a2ecf9693fcb3ddae62d301476ddc0ae36e
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-09-08 10:04:32 +0200

    Added adaptive variance integrator

--- zunis_lib/zunis/integration/adaptive_survey_integrator.py
+++ zunis_lib/zunis/integration/adaptive_survey_integrator.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from better_abc import abstractmethod
 import torch
 from .flat_survey_integrator import FlatSurveySamplingIntegrator
@@ -85,7 +86,7 @@ class ForwardSurveySamplingIntegrator(AdaptiveSurveyIntegrator):
 
     def __init__(self, f, trainer, d, n_iter=10, n_iter_survey=None, n_iter_refine=None,
                  n_points=100000, n_points_survey=None, n_points_refine=None, use_survey=False,
-                 device=torch.device("cpu"), verbosity=2, trainer_verbosity=1, sample_flat_once=False , **kwargs):
+                 device=torch.device("cpu"), verbosity=2, trainer_verbosity=1, sample_flat_once=False, **kwargs):
         super(ForwardSurveySamplingIntegrator, self).__init__(f,
                                                               trainer,
                                                               d,
@@ -105,3 +106,50 @@ class ForwardSurveySamplingIntegrator(AdaptiveSurveyIntegrator):
 
     def survey_switch_condition(self):
         return True
+
+
+class VarianceAdaptiveSurveyIntegrator(AdaptiveSurveyIntegrator):
+    """Survey/Refine adaptive integrator based on the variance loss.
+    The integrator estimates the variance of a flat integrator and switches to forward sampling if
+    the flow performs significantly (2 sigma) better.
+    """
+
+    def __init__(self, f, trainer, d, n_iter=10, n_iter_survey=None, n_iter_refine=None,
+                 n_points=100000, n_points_survey=None, n_points_refine=None, use_survey=False,
+                 device=torch.device("cpu"), verbosity=2, trainer_verbosity=1, **kwargs):
+        self.flat_loss = None
+        self.flat_loss_std = None
+        super(VarianceAdaptiveSurveyIntegrator, self).__init__(f,
+                                                               trainer,
+                                                               d,
+                                                               n_iter=n_iter,
+                                                               n_iter_survey=n_iter_survey,
+                                                               n_iter_refine=n_iter_refine,
+                                                               n_points=n_points,
+                                                               n_points_survey=n_points_survey,
+                                                               n_points_refine=n_points_refine,
+                                                               use_survey=use_survey,
+                                                               device=device,
+                                                               verbosity=verbosity,
+                                                               trainer_verbosity=trainer_verbosity,
+                                                               **kwargs)
+
+    def compute_flat_variance_loss(self, fx):
+        """Compute the variance loss and its standard deviation assuming points are sampled from a flat distribution
+        We clip the loss standard deviation to loss/4 so that the switching condition can be met
+        """
+        losses = fx ** 2
+        self.flat_loss = losses.mean().cpu().item()
+        self.flat_loss_std = min(self.flat_loss/4., losses.std().cpu().item())
+
+    def survey_switch_condition(self):
+        return self.model_trainer.record["loss"] < self.flat_loss - self.flat_loss_std
+
+    def process_survey_step(self, sample, integral, integral_var, training_record, **kwargs):
+        if not self.sample_forward:
+            _, _, fx = sample
+            self.compute_flat_variance_loss(fx)
+
+        super(VarianceAdaptiveSurveyIntegrator, self).process_survey_step(sample, integral, integral_var,
+                                                                          training_record,
+                                                                          **kwargs)
