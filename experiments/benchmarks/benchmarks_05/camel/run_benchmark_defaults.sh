95d8158b57bda789f218e98873fec9724effb2c0
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2021-09-16 14:22:14 +0200

    updated default camel runs

--- experiments/benchmarks/benchmarks_05/camel/run_benchmark_defaults.sh
+++ experiments/benchmarks/benchmarks_05/camel/run_benchmark_defaults.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sleep 0; python benchmark_camel.py --config benchmarks_05/camel/camel_2d_defaults.yaml --cuda=0 --n_search=10 --stratified&
-sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_4d_defaults.yaml --cuda=1 --n_search=10 --stratified&
-sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_8d_defaults.yaml --cuda=2 --n_search=10 --stratified&
-sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_16d_defaults.yaml --cuda=3 --n_search=10 --stratified&
-sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_32d_defaults.yaml --cuda=4 --n_search=10 --stratified&
-sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_32d_histat_defaults.yaml --cuda=5 --n_search=10 --stratified&
+python benchmark_camel.py --config benchmarks_05/camel/camel_2d_defaults.yaml --cuda=0 --n_repeat=10 --stratified&
+sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_4d_defaults.yaml --cuda=1 --n_repeat=10 --stratified&
+sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_8d_defaults.yaml --cuda=2 --n_repeat=10 --stratified&
+sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_16d_defaults.yaml --cuda=3 --n_repeat=10 --stratified&
+sleep 3; python benchmark_camel.py --config benchmarks_05/camel/camel_32d_defaults.yaml --cuda=4 --n_repeat=10 --stratified&
+#sleep 3; python benchmark_camel.py --config benchmarks_03/camel/camel_32d_defaults_highstat.yaml --cuda=5 --n_repeat=10&
 
 # wait until all child processes terminate
 wait
