a19316fd49ab8c2a4b5ea16920cb1af9c43cde6a
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-09-17 10:07:49 +0200

    Updated speed measurement command file

--- experiments/benchmarks/benchmarks_04/camel/run_benchmark_defaults_speed.sh
+++ experiments/benchmarks/benchmarks_04/camel/run_benchmark_defaults_speed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python benchmark_camel.py --config benchmarks_04/camel/camel_2d_defaults.yaml --cuda=5 --n_search=1&
-sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_4d_defaults.yaml --cuda=1 --n_search=1&
-sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_8d_defaults.yaml --cuda=2 --n_search=1&
-sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_16d_defaults.yaml --cuda=3 --n_search=1&
-sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_32d_defaults.yaml --cuda=4 --n_search=1&
+python benchmark_camel.py --config benchmarks_04/camel/camel_2d_defaults.yaml --cuda=5 --n_repeat=1&
+sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_4d_defaults.yaml --cuda=1 --n_repeat=1&
+sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_8d_defaults.yaml --cuda=2 --n_repeat=1&
+sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_16d_defaults.yaml --cuda=3 --n_repeat=1&
+sleep 3; python benchmark_camel.py --config benchmarks_04/camel/camel_32d_defaults.yaml --cuda=4 --n_repeat=1&
 
 # wait until all child processes terminate
 wait
