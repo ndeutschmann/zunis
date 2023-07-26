9d1b4c0ad032f587f8ab48a78655a571128ef3f9
Niklas Götz <goetz@fias.uni-frankfurt.de>
2023-07-26 10:08:17 +0200

    Fixed hypersphere

--- experiments/benchmarks/benchmark_hypersphere.py
+++ experiments/benchmarks/benchmark_hypersphere.py
@@ -25,13 +25,16 @@ def hypersphere_benchmark(dimensions=(2, 4, 6, 8), rs=(0.3, 0.4, 0.5), cs=(0.5,)
     if debug:
         base_integrator_config["n_epochs"] = 1
         base_integrator_config["n_iter"] = 1
-    #TODO TEST, GET GRID WORKING, CHECK OTHER BENCHMARKS
+    #TODO where to put integrator config? where to put grid 
+    # TEST, GET GRID WORKING, CHECK OTHER BENCHMARKS
     benchmarker=KnownIntegrandGridBenchmarker()
     benchmark_config = benchmarker.set_benchmark_grid_config(dimensions=dimensions,
                                        base_integrand_params=base_integrand_params,
                                        n_batch=100000, debug=debug, cuda=cuda, 
                                        dbname="benchmarks.db", experiment_name="hypersphere")
-    benchmarker.run(integrand=RegulatedHyperSphereIntegrand, sql_dtypes=dtypes,**benchmark_config)
+    benchmarker.set_benchmark_grid_config_param(benchmark_config, "integrand_params_grid", integrands_params_grid, None)
+    benchmarker.set_benchmark_grid_config_param(benchmark_config, "base_integrator_config", base_integrator_config, None)
+    benchmarker.run(integrand=RegulatedHyperSphereIntegrand, sql_dtypes=dtypes, **benchmark_config)
 
 
 cli = click.Command("cli", callback=hypersphere_benchmark, params=[
