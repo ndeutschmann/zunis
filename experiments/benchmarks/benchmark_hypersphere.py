1e10ed44c944cc5a54fb0d4cf5a951608ddc2b21
Niklas Götz <goetz@fias.uni-frankfurt.de>
2023-07-26 10:14:37 +0200

    Fix hyperrect

--- experiments/benchmarks/benchmark_hypersphere.py
+++ experiments/benchmarks/benchmark_hypersphere.py
@@ -25,8 +25,7 @@ def hypersphere_benchmark(dimensions=(2, 4, 6, 8), rs=(0.3, 0.4, 0.5), cs=(0.5,)
     if debug:
         base_integrator_config["n_epochs"] = 1
         base_integrator_config["n_iter"] = 1
-    #TODO where to put integrator config? where to put grid 
-    # TEST, GET GRID WORKING, CHECK OTHER BENCHMARKS
+    
     benchmarker=KnownIntegrandGridBenchmarker()
     benchmark_config = benchmarker.set_benchmark_grid_config(dimensions=dimensions,
                                        base_integrand_params=base_integrand_params,
